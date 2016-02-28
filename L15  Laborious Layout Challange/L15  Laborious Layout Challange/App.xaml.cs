11f8d6b74f19f95360341bcdc27b45f0df867c1c
Elijah C Menifee <ElijahCMenifee@gmail.com>
2016-02-28 02:06:32 -0600

    Close enough, seeing requirements don't specify exact spacing, too much trouble to get it exact like screenshot, is very close...

--- L15  Laborious Layout Challange/L15  Laborious Layout Challange/App.xaml.cs
+++ L15  Laborious Layout Challange/L15  Laborious Layout Challange/App.xaml.cs
@@ -43,12 +43,12 @@ namespace L15__Laborious_Layout_Challange
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
+//#if DEBUG
+//            if (System.Diagnostics.Debugger.IsAttached)
+//            {
+//                this.DebugSettings.EnableFrameRateCounter = true;
+//            }
+//#endif
 
             Frame rootFrame = Window.Current.Content as Frame;
 
